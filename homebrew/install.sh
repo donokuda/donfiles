56cae72444549a5efc342fbaa1740eeb778cf0a4
Don Okuda <don.okuda@gmail.com>
2013-07-16 09:50:46 -0700

    Homebrew install sh

---
+++ homebrew/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least
+# desired) using homebrew
+
+if test ! $(which brew)
+then
+  echo "  x You should probably install Homebrew first:"
+  echo "    https://github.com/mxcl/homebrew/wiki/installation"
+  exit
+fi
+
+# Install homebrew packages
+brew install grc coreutils spark
+
+exit 0
