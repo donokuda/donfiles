ba55c3bffdcfa7305d17e14d290e539614b29c91
Don Okuda <don.okuda@gmail.com>
2013-07-17 09:26:58 -0700

    Move tmux color alias to tmux/aliases.zsh

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,3 +1,6 @@
+# Load tmux with the right colors
+alias tmux="TERM=screen-256color-bce tmux"
+#
 # Alias reattach-to-user-namespace
 # `brew intall reattach-to-user-namespace`
 alias ratt="reattach-to-user-namespace"
