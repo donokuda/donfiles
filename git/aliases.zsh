a6bc6bbee3b2c36d88445044054b59b67e7c6846
Don Okuda <don.okuda@gmail.com>
2013-07-18 16:00:52 -0700

    Add some handy git aliases shown to me by a coworker

---
+++ git/aliases.zsh
@@ -0,0 +1,2 @@
+alias gl="git log --pretty=format:'%C(yellow)%h%Creset -%C(red)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset' --abbrev-commit"
+alias glg="git log --graph --pretty=format:'%C(yellow)%h%Creset -%C(red)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset' --abbrev-commit"
