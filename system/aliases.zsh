f3440081e1e259ae38db4d1bf6b8ff3849bd0938
Don Okuda <don.okuda@gmail.com>
2013-08-06 16:27:17 -0700

    Alias `alert` to notify that a task is done.
    thx @eric-hu

---
+++ system/aliases.zsh
@@ -0,0 +1,6 @@
+# Use to alert that a task is complete
+# Example:
+#   $ brew install erlang-r16; alert
+#   (...30 minutes later...)
+#   =>  *BEEP!* "MEE YUNG KYU CHOI" *BEEP!*
+alias alert='tput bel; say mee yung kyu choi; tput bel;'
