836877fd9f33106385b9f4fbb9bbffd0bda23a87
Don Okuda <don.okuda@gmail.com>
2013-08-12 16:33:32 -0700

    Alias `git` to `hub` if hub is installed

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,2 +1,6 @@
 alias gl="git log --pretty=format:'%C(yellow)%h%Creset -%C(red)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset' --abbrev-commit"
 alias glg="git log --graph --pretty=format:'%C(yellow)%h%Creset -%C(red)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset' --abbrev-commit"
+
+# Alias `git` to `hub` if github's hub CLI is installed
+# https://github.com/github/hub
+if [[ -x hub ]]; then alias git='hub'; fi
